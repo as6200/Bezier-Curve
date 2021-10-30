ee8a375ff0990f828569a6dff4e7409e6ecff720
as6200 <aayush6200@outlook.com>
2021-10-30 09:02:57 -0500

    Basic logic

--- main.js
+++ main.js
@@ -17,8 +17,14 @@ function makePoint(e) {
         };
 	};
 
-	let pos = getMousePos(canvas, e);
-	points.push([pos.x, pos.y]);
+	let pos = getMousePos(canvas, e); // Gets mouse position
+
+	if (pos.x > 20 && pos.y > 10 && pos.x < 100 && pos.y < 110) {
+		makeCurve();
+	} else {
+		points.push([pos.x, pos.y]); // adds it to points
+	}
+	
 }
 
 
@@ -44,16 +50,68 @@ function drawLine(x1, y1, x2, y2) {
 }
 
 
+function draw() {
+	// Start Button
+	ctx.beginPath();
+	ctx.rect(20, 10, 80, 100);
+	ctx.fillStyle = "#BF565A";
+	ctx.fill();
+	ctx.lineWidth = 1;
+	ctx.strokeStyle = 'black';
+	ctx.stroke();
+
+	ctx.beginPath();
+	ctx.moveTo(40, 20);
+	ctx.lineTo(90, 55);
+	ctx.lineTo(40, 95);
+	ctx.lineTo(40, 20);
+	ctx.fillStyle = '#800000';
+	ctx.fill();
+	ctx.strokeStyle = 'black';
+	ctx.stroke();
+
+	for (let i = 0; i < points.length; i++) {
+		drawPoint(points[i][0], points[i][1]);
+	}
+
+	for (let i = 0; i < points.length - 1; i++) {
+		drawLine(points[i][0], points[i][1], points[i+1][0], points[i+1][1]);
+	}
+}
+
+
+function getPoints(x0, y0, x1, y1) {
+	let subPoints = []
+	for (let t = 0; t <= 1; t+= 0.01) {
+		ctx.beginPath();
+		let x = (1-t) * x0 + t * x1;
+		let y = (1-t) * y0 + t * y1;
+		subPoints.push([x, y]);
+	}
+	return subPoints;
+}
+
+
+function makeCurve() {
+	let allPoints = [];
+	for (let i = 0; i < points.length - 1; i++) {
+		allPoints.push(getPoints(points[0+i][0], points[0+i][1], points[1+i][0], points[1+i][1]));
+		for (let j = 0; j < allPoints[i].length; j++) {
+			ctx.arc(allPoints[i][j][0], allPoints[i][j][1], 1, 0, 2 * Math.PI, false);
+			ctx.fillStyle = 'black';
+			ctx.fill();
+			ctx.lineWidth = 3;
+			ctx.strokeStyle = 'black';
+			ctx.stroke();
+		}
+	}
+}
+
+
 function main () {
 	// Main loop
     function gameLoop () {
-		for (let i = 0; i < points.length; i++) {
-			drawPoint(points[i][0], points[i][1]);
-		}
-
-		for (let i = 0; i < points.length - 1; i++) {
-			drawLine(points[i][0], points[i][1], points[i+1][0], points[i+1][1]);
-		}
+		draw();
 
 		window.requestAnimationFrame(gameLoop, canvas); // Loops it
         
@@ -63,5 +121,4 @@ function main () {
 }
 
 window.addEventListener("mousedown", makePoint, false);
-
 main();
