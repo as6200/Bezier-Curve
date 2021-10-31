8b943b64e2016afd30e424cf212d8d7587b338ae
as6200 <aayush6200@outlook.com>
2021-10-30 22:18:19 -0500

    commented

--- main.js
+++ main.js
@@ -3,13 +3,14 @@ const ctx = canvas.getContext("2d"); // Gets context
 // Sets Width and Height
 ctx.canvas.width  = window.innerWidth;
 ctx.canvas.height = window.innerHeight;
-var points = [];
-ctx.fillStyle = '#add8e6';
-ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-var showDetail = false;
+var points = []; // User created points
+var showCurve = false; // Bezier-Curve toggle (TODO)
+var BPoints = []; // Points in Bezier Curve
 
 
-function makePoint(e) {
+// Handles mouseDown
+function mouseDownHandler(e) {
+	// Function to get mouse position
 	let getMousePos = function (canvas, evt) {
         var rect = canvas.getBoundingClientRect();
         return {
@@ -21,14 +22,17 @@ function makePoint(e) {
 	let pos = getMousePos(canvas, e); // Gets mouse position
 
 	if (pos.x > 20 && pos.y > 10 && pos.x < 100 && pos.y < 110) {
-		makeCurve();
+		// If start button is clicked, make curve
+		BPoints = makeCurve();
 	} else {
-		points.push({x:pos.x, y:pos.y}); // adds it to points
+		// Else, adds mouse pos as a point
+		points.push({x:pos.x, y:pos.y});
 	}
 	
 }
 
 
+// Draw a point
 function drawPoint(x, y) {
 	ctx.beginPath();
 	ctx.arc(x, y, 5, 0, 2 * Math.PI, false);
@@ -40,6 +44,7 @@ function drawPoint(x, y) {
 }
 
 
+// Draws a line
 function drawLine(x1, y1, x2, y2) {
 	ctx.lineWidth = 1;
 	ctx.strokeStyle = 'black';
@@ -51,7 +56,12 @@ function drawLine(x1, y1, x2, y2) {
 }
 
 
+// Draws everything else
 function draw() {
+	// Background
+	ctx.fillStyle = '#add8e6';
+	ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
 	// Start Button
 	ctx.beginPath();
 	ctx.rect(20, 10, 80, 100);
@@ -61,6 +71,7 @@ function draw() {
 	ctx.strokeStyle = 'black';
 	ctx.stroke();
 
+	// Start Button Box
 	ctx.beginPath();
 	ctx.moveTo(40, 20);
 	ctx.lineTo(90, 55);
@@ -71,47 +82,62 @@ function draw() {
 	ctx.strokeStyle = 'black';
 	ctx.stroke();
 
+	// Draws User Points
 	for (let i = 0; i < points.length; i++) {
 		drawPoint(points[i].x, points[i].y);
 	}
 
+	//Draws lines between points
 	for (let i = 0; i < points.length - 1; i++) {
 		drawLine(points[i].x, points[i].y, points[i+1].x, points[i+1].y);
 	}
-}
 
-
-function getPoint(t, sPoints) {
-	let subPoints = [];
-	if (sPoints.length > 1) {
-		for (let i = 0; i < sPoints.length - 1; i++) {
-			let x = sPoints[i].x + t * (sPoints[i + 1].x - sPoints[i].x);
-			let y = sPoints[i].y + t * (sPoints[i + 1].y - sPoints[i].y);
-
-			subPoints.push({x: x, y: y});
-		}
-
-		return getPoint(t, subPoints);
-	} else {
-		return sPoints[0];
-	}
-
-}
-
-
-function makeCurve() {
-	let BPoints = [];
-	for (let t = 0; t < 1; t += 0.001) {
-		BPoints.push(getPoint(t, points))
-	}
-
-	for (let i = 0; i < BPoints.length; i++) {
+	// Draws Bezier Curve
+	for (let i = 0; i < BPoints.length - 1; i++) {
 		ctx.beginPath();
 		ctx.arc(BPoints[i].x, BPoints[i].y, 1, 0, 2 * Math.PI, false);
 		ctx.lineWidth = 3;
 		ctx.strokeStyle = 'black';
 		ctx.stroke();
 	}
+}
+
+
+// Recursive function to get bezier curve points
+function getPoint(t, sPoints) {
+	let subPoints = [];
+	if (sPoints.length > 1) {
+		// Finds points for all lines made with points given
+		for (let i = 0; i < sPoints.length - 1; i++) {
+
+			// Bezier Formula: P0 + t(P1 - P0)
+			let x = sPoints[i].x + t * (sPoints[i + 1].x - sPoints[i].x);
+			let y = sPoints[i].y + t * (sPoints[i + 1].y - sPoints[i].y);
+
+			// Adds to subpoints
+			subPoints.push({x: x, y: y});
+		}
+
+		// passes subPoints for next points and recursion will give final point
+		return getPoint(t, subPoints);
+	} else {
+
+		// This is the final point
+		return sPoints[0];
+	}
+
+}
+
+
+// Makes the Bezier Curve
+function makeCurve() {
+	somePoints = []; // idk
+
+	// Gets all points for t and stores in somePoints
+	for (let t = 0; t < 1; t += 0.001) {
+		somePoints.push(getPoint(t, points))
+	}
+	return somePoints;
 }
 
 
@@ -127,5 +153,5 @@ function main () {
     window.requestAnimationFrame(gameLoop, canvas); // Starts loop
 }
 
-window.addEventListener("mousedown", makePoint, false);
+window.addEventListener("mousedown", mouseDownHandler, false); 
 main();
